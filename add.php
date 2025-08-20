672a2bfb522d941e28000396843880e85c00695d
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-08-20 21:47:51 +0100

    add.php code ENGR. IPAYE

--- add.php
+++ add.php
@@ -1,1 +1,12 @@
 <?php
+global $conn;
+include 'config.php';
+
+if(isset($_POST['title'])) {
+    $title = $conn->real_escape_string($_POST['title']);
+    $conn->query("INSERT INTO tasks (title) VALUES ('$title')");
+
+}
+header("Location: index.php");
+exit;
+?>
--- config.php
+++ config.php
@@ -11,3 +11,4 @@ $conn = new mysqli($host, $user, $pass, $db);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
+?>
