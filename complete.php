3764d8bec49671017496f68f3c54ed91fed653da
Ipaye Babatunde <fineboyhakim@gmail.com>
2025-08-20 21:53:11 +0100

    COMPLETE AND DELETE code ENGR. IPAYE

--- complete.php
+++ complete.php
@@ -1,1 +1,12 @@
 <?php
+global $conn;
+include 'config.php';
+
+if(isset($_GET['id'])) {
+    $id = (int) $_GET['id'];
+    $conn->query("UPDATE tasks SET is_completed = TRUE WHERE id= $id");
+
+}
+header("Location: index.php");
+exit;
+?>
--- delete.php
+++ delete.php
@@ -1,1 +1,12 @@
 <?php
+global $conn;
+include 'config.php';
+
+if(isset($_GET['id'])) {
+    $id = (int) $_GET['id'];
+    $conn->query("DELETE FROM tasks WHERE  id=$id");
+
+}
+header("Location: index.php");
+exit;
+?>
